81e4290a117a463de0597e107666663c2bfab944
michael.stedler <m_stedler@gmx.de>
2017-11-02 13:14:43 +0100

    Crawler now finds project hrefs and href to next page of search results

--- src/datasource-ulule.py
+++ src/datasource-ulule.py
@@ -12,7 +12,6 @@ Update:
 - Ulule API only works for own projects
 
 """
-
 regex = re.compile(
         r'^(?:http|ftp)s?://' # http:// or https://
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
@@ -30,40 +29,55 @@ class DataSourceUlule():
         self.searchterms   = searchterms
         self.crawled_hrefs = []
 
+    # Check if URL matches all requirements for being a real URL
+    # @return: TRUE if url meets regex requirements
     def isValidUrl(self,url):
         if regex.match(url) is not None:
             return True;
         return False
 
+    # Crawls webpage for every defined searchterm
+    # @return: list of all crawled URLs
     def crawler(self):
-        tocrawl = [self.URL]
+
         crawled = []
-        while tocrawl:
 
-            page = tocrawl.pop()
+        for term in self.searchterms:
 
-            print 'Crawled:' + page
+            tocrawl = [self.SEARCH_URL + term]
 
-            pagesource = urllib2.urlopen(page)
-            s = pagesource.read()
+            while tocrawl:
 
-            soup = BeautifulSoup(s, "html5lib")
+                page = tocrawl.pop()
 
-            links = soup.findAll('a', href=True)
+                print 'Crawled:' + page
 
-            if page not in crawled:
-                for l in links:
-                    if self.isValidUrl(l['href']):
-                        tocrawl.append(l['href'])
+                pagesource = urllib2.urlopen(page)
+                s = pagesource.read()
 
-                crawled.append(page)
+                soup = BeautifulSoup(s, "html5lib")
+
+                # find all ulule project hrefs
+                # b-blink__link html class for projects
+                rawLinks = soup.findAll('a', href=True, class_="b-blink__link")
+
+                # find next page in search results
+                nextPage = soup.findAll('a', href=True, class_="page")[0]
+
+                # Crawling deeper with found hrefs
+                # if page not in crawled:
+                #     for l in links:
+                #         if self.isValidUrl(l['href']):
+                #             tocrawl.append(l['href'])
+                #
+                #     crawled.append(page)
 
         return crawled
 
 '''
 Testing
 '''
-searchterms = ['ngo', 'non-governmental']
+searchterms = ['ngo']
 
 obj = DataSourceUlule(searchterms)
 
