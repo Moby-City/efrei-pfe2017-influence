9dde58e488ca88a3ef9bce7f3ded0c06d93a1ae2
Tom Beckmann <tomjonabc@gmail.com>
2017-11-03 12:34:31 +0100

    Move ulule search to python3 and to /all endpoint

--- src/organization.py
+++ src/organization.py
@@ -1,10 +1,12 @@
-/orgranization.py
+
 class Organization():
 
     def __init__(self,
             title, 
+            url,
             description='',
             articles=[]):
         self.title = title
+        self.url = url
         self.description = description
         self.articles = articles
