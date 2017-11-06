59a79c09e32a224e21468ca10cb97b661685e857
Corinna Jaschek <corinna.jaschek@gmail.com>
2017-11-06 13:17:55 +0100

    finish crawling carenews, fixes #10

--- src/datasource-carenews.py
+++ src/datasource-carenews.py
@@ -3,37 +3,52 @@ import urllib3
 from bs4 import BeautifulSoup
 import newspaper
 from organization import Organization
+import json
 
 http = urllib3.PoolManager()
 
 URL = 'http://www.carenews.com/fr/organisations'
+
+def json_serial(obj):
+    return obj.__dict__
 
 class DataSourceCareNews(DataSource):
     def findAll(self):
         all_organizations = []
 
         result = self.requestUrl(URL)
-        organisations_list = BeautifulSoup(result, 'html.parser').select_one('.organizations-list-items')
+        organizations_list = BeautifulSoup(result, 'html.parser').select_one('.organizations-list-items')
 
-        organization_urls = self.parseSearchResult(organisations_list)
+        organization_urls = self.parseSearchResult(organizations_list)
         for url in organization_urls:
             result = self.requestUrl(url)
-            self.parseOrganizationPage(BeautifulSoup(result, 'html.parser'))
+            description = self.parseOrganizationDescription(BeautifulSoup(result, 'html.parser'))
+            title = self.parseOrganizationTitle(BeautifulSoup(result, 'html.parser'))
+            all_organizations.append(Organization(title = title, description = description, url = url))
 
+            self.writeOrganizationList(all_organizations, 'carenews.json')
 
     def parseSearchResult(self, rootElement):
         result_list = []
-        for organisation in rootElement.select('.post header .organization_data_link'):
-            url = organisation['href']
+        for organization in rootElement.select('.post header .organization_data_link'):
+            url = organization['href']
             result_list.append(url)
 
         return result_list
 
-    def parseOrganizationPage(self, page):
-        description = page.select('.description span')
-        print(description)
+    def parseOrganizationDescription(self, page):
+        span = page.select('.description span')
+        return span[0].text.strip()
+
+    def parseOrganizationTitle(self, page):
+        title = page.select('.big h1')
+        return title[0].text
 
     def requestUrl(self, url):
         return http.request('GET', url).data.decode('utf-8')
+
+    def writeOrganizationList(self, organizations, filename):
+        f = open(filename, 'w')
+        f.write(json.dumps(organizations, default=json_serial))
 
 DataSourceCareNews().findAll()
