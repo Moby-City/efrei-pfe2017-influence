1dca451339cc41f0862245c755e688882df563b4
Corinna Jaschek <corinna.jaschek@gmail.com>
2017-11-27 11:06:35 +0100

    remove debug output

--- crawl/datasources/afp.py
+++ crawl/datasources/afp.py
@@ -12,11 +12,8 @@ class DataSourceAFP(DataSource):
 
     def find_all(self):
         afp = newspaper.build('https://www.afp.com/fr', memoize_articles=False)
-
-        print(afp.category_urls())
-        print(afp.size())
+        
         now = datetime.datetime.now()
-        done = 0
 
         self.add_all_results([DataSet(None, a.url, now, self) for a in afp.articles if '/fr/' in a.url])
 
